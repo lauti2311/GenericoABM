ed9301380a26a616267890223ac2d566f975c140
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-05-01 13:11:37 -0300

    Todavia no descubro que es ese error de any y function

---
+++ src/services/GenericBackendClient.ts
@@ -0,0 +1,20 @@
+import  DataModel  from "../types/DataModel";
+
+abstract class GenericBackendClient<T extends DataModel<T>> {
+    protected baseUrl: string;
+  
+    constructor(baseUrl: string) {
+      this.baseUrl = baseUrl;
+    }
+  
+    abstract getAll(): Promise<T[]>;
+  
+    abstract getById(id: number): Promise<T | null>;
+  
+    abstract post(data: T): Promise<T>;
+  
+    abstract put(id: number, data: T): Promise<T>;
+  }
+  
+  
+  export default GenericBackendClient;
